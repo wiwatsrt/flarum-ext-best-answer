4cdfe71b8244e2eb83e280423bb92512468262b1
David Sevilla Martin <dsevilla192@icloud.com>
2019-08-09 11:30:56 -0400

    Fix unable to remove best answer

--- src/Listener/SelectBestAnswers.php
+++ src/Listener/SelectBestAnswers.php
@@ -27,14 +27,20 @@ class SelectBestAnswers
         if (!Arr::has($event->data, $this->key)) return;
 
         $discussion = $event->discussion;
-        $data = $event->data;
-        $id = (int) Arr::get($data, $this->key);
+        $id = (int) Arr::get($event->data, $this->key);
 
-        if ($id > 0 && $discussion->exists && isset($id) && $event->actor->can('selectBestAnswerInDiscussion', $discussion) && $discussion->best_answer_post_id != $id) {
+        if (!isset($id) || !$discussion->exists || $discussion->best_answer_post_id == $id || $event->actor->cannot('selectBestAnswerInDiscussion', $discussion)) {
+            return;
+        }
+
+        if ($id > 0) {
             $discussion->best_answer_post_id = $id;
             $discussion->best_answer_user_id = $event->actor->id;
 
             Notification::where('type', 'selectBestAnswer')->where('subject_id', $discussion->id)->delete();
+        } else if ($id == 0) {
+            $discussion->best_answer_post_id = null;
+            $discussion->best_answer_user_id = null;
         }
     }
 }
